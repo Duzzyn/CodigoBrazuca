3fdb2c55393c40aec6e334cbc241a1f77c8394f6
Duzzyn <eduardonense05@gmail.com>
2025-02-21 03:36:18 -0300

    :hammer: Adicionando projeto saldo transacoes

--- Semana_05_TypeScript/Saldo_Transacoes-TS/src/app.ts
+++ Semana_05_TypeScript/Saldo_Transacoes-TS/src/app.ts
@@ -1,17 +1,23 @@
 import { ethers } from "ethers"
 
-const provider = ethers.getDefaultProvider("mainnet");
+const provider = new ethers.providers.InfuraProvider("homestead", "26e815f149f94189ab52bee28453bc2d")
+const keyEtherscan = "ZIY8XK8M8D1HKG41T7W6JE2PAY56UKHTNC"
+
 
 export function initApp() {
     const walletInput = document.getElementById("wallet-address") as HTMLInputElement;
     const balanceDisplay = document.getElementById("balance") as HTMLParagraphElement;
     const transactionsDisplay = document.getElementById("transactions") as HTMLDivElement;
-    const checkBalanceButton = document.getElementById("checkbalance") as HTMLButtonElement;
-    const checkTransactionsButton = document.getElementById("checktransactions") as HTMLButtonElement;
+    const checkBalanceButton = document.getElementById("check-balance") as HTMLButtonElement;
+    const checkTransactionsButton = document.getElementById("check-transactions") as HTMLButtonElement;
+    const selectValue = document.getElementById("select") as HTMLSelectElement;
+    
+    async function updateNetwork(): Promise<void> {
+        const networkSelect = selectValue.value
+    }
 
     checkBalanceButton.addEventListener("click", async () => {
         const address = walletInput.value.trim();
-
         if (!ethers.utils.isAddress(address)) {
             balanceDisplay.textContent = "Endereço inválido!";
             return;
@@ -32,13 +38,28 @@ export function initApp() {
             return;
         }
         try {
-            const history = await provider.getHistory(address);
-            transactionsDisplay.innerHTML = "<h3>Últimas Transações: </h3>";
-            history.slice(0, 5).forEach((tx: { from: any; to: any; value: any; }) => {
-                const txElement = document.createElement("p");
-                txElement.textContent = `De: ${tx.from} Para: ${tx.to} - Valor: ${ethers.utils.formatEther(tx.value)} ETH`;
-                transactionsDisplay.appendChild(txElement);
-            });
+            const response = await fetch(
+                `https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&sort=asc&apikey=${keyEtherscan}`
+            );
+            const data = await response.json();
+            console.log(data)
+            if(data.status === "1" && data.result.length > 0) {
+                data.result.forEach((tx: any, index: number) => {
+                    const transacaoInfo = `
+                    <h2 class="title is-2">Últimas Transações: ${index + 1}</h2>
+                    <ul>
+                      <li><strong>De:</strong> ${tx.from}</li>
+                      <li><strong>Para:</strong> ${tx.to}</li>
+                      <li><strong>Valor:</strong> ${ethers.utils.formatEther(tx.value)} Eth</li>
+                      <li><strong>Data:</strong> ${new Date(parseInt(tx.timeStamp) * 1000).toLocaleString()}</li>
+                    </ul>
+                    <hr />
+                  `;
+                  transactionsDisplay.innerHTML = transacaoInfo;
+                })
+            } else {
+                console.error(data.message);     
+            }
         } catch (error) {
             transactionsDisplay.textContent = "Erro ao buscar as transações.";
             console.error(error);
